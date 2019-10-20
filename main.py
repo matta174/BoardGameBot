8d5db249b4b6df832b3fc30cdc9fb291a36e2c00
Matthew Compton <matthewc174@gmail.com>
2019-10-20 17:12:21 -0400

    added error handling for all methods and commented scoring code for time being
    Signed-off-by: Matthew Compton <matthewc174@gmail.com>

--- main.py
+++ main.py
@@ -47,6 +47,13 @@ async def bgg_check(ctx, *, gamename):
         return
     await ctx.send(main_response,embed=embed)
 
+@bgg_check.error
+async def bgg_check_error(ctx, error):
+    if isinstance(error, BaseException):
+        await ctx.send('Unexpected error, try again. If the error persists,'
+                       ' get help here https://discord.gg/9pS2JdC')
+        logger.error(error, exc_info=True)
+
 
 @client.command(name='Expansion_Check',
                 description="Returns expansions for the selected game if any",
@@ -57,6 +64,13 @@ async def expansion_check(ctx, *, game):
     main_response = Python.BGG.game_expansion(game)
     await ctx.send(main_response)
 
+@expansion_check.error
+async def expansion_check_error(ctx, error):
+    if isinstance(error, BaseException):
+        await ctx.send('Unexpected error, try again. If the error persists,'
+                       ' get help here https://discord.gg/9pS2JdC')
+        logger.error(error, exc_info=True)    
+
 
 @client.command(name='Random_Game',
                 description="Returns a random game title from a provided list",
@@ -66,6 +80,13 @@ async def expansion_check(ctx, *, game):
 async def random_game(ctx, *, arg):
     possible_responses = arg.split(',')
     await ctx.send(random.choice(possible_responses))
+
+@random_game.error
+async def random_game_error(ctx, error):
+    if isinstance(error, BaseException):
+        await ctx.send('Unexpected error, try again. If the error persists,'
+                       ' get help here https://discord.gg/9pS2JdC')
+        logger.error(error, exc_info=True)    
 
 
 @client.command(name='Random_Owned_Game',
@@ -79,6 +100,12 @@ async def random_users_game(ctx, name):
     random_game_name = Python.BGG.random_owned_game(name)
     await ctx.send(random_game_name)
 
+@random_users_game.error
+async def random_users_game_error(ctx, error):
+    if isinstance(error, BaseException):
+        await ctx.send('Unexpected error, try again. If the error persists,'
+                       ' get help here https://discord.gg/9pS2JdC')
+        logger.error(error, exc_info=True)    
 
 @client.command(name='What_Game_Can_We_Play',
                 description="Looks up a user's collection and how many people are playing to see what games you could play",
@@ -92,98 +119,105 @@ async def what_game_can_we_play(ctx, *, arg):
     games_we_can_play = Python.BGG.what_games_can_we_play(name, number_of_players)
     await ctx.send(games_we_can_play)
 
-
-@client.command(name='Get_All_Wins',
-                description="Gets the wins of every user for every game",
-                brief="Checks user's scores",
-                aliases=['getwins', 'get_wins', 'gw']
-                )
-async def get_all_wins(ctx, member: typing.Optional[discord.Member] = None, *, arg=None):
-    response = Python.data_storage.get_wins(ctx, member, arg)
-    await ctx.send(response)
-
-
-@get_all_wins.error
-async def get_all_wins_error(ctx, error):
+@what_game_can_we_play.error
+async def what_game_can_we_play_error(ctx, error):
     if isinstance(error, BaseException):
-        await ctx.send('Unexpected error retrieving wins, try again. If the error persists,'
+        await ctx.send('Unexpected error, try again. If the error persists,'
                        ' get help here https://discord.gg/9pS2JdC')
-        logger.error(error, exc_info=True)
+        logger.error(error, exc_info=True)        
 
 
-@client.command(name='Add_Win',
-                description="Adds a win to the user's total for a game",
-                brief="Adds a win to ther user for a game",
-                aliases=['addwin', 'add_win', 'aw', 'win'],
-                )
-async def add_win(ctx, member: discord.Member, *, game_name):
-    response = Python.data_storage.add_win_db(ctx, member, game_name)
-    await ctx.send(response)
+# @client.command(name='Get_All_Wins',
+#                 description="Gets the wins of every user for every game",
+#                 brief="Checks user's scores",
+#                 aliases=['getwins', 'get_wins', 'gw']
+#                 )
+# async def get_all_wins(ctx, member: typing.Optional[discord.Member] = None, *, arg=None):
+#     response = Python.data_storage.get_wins(ctx, member, arg)
+#     await ctx.send(response)
 
 
-@add_win.error
-async def add_win_error(ctx, error):
-    if isinstance(error, discord.ext.commands.BadArgument):
-        await ctx.send('Failed to add win. Make sure you are using the format `!aw [user] [boardgame name]`'
-                       ' and that you are mentioning the user directly with @.')
-        logger.error(error, exc_info=True)
-    elif isinstance(error, discord.ext.commands.MissingRequiredArgument):
-        await ctx.send('Failed to add win as you did not provide either the user who won or the name of the game. '
-                       'Make sure you are using the format `!aw [user] [boardgame name]`'
-                       ' and that you are mentioning the user directly with @.')
-        logger.error(error, exc_info=True)
-    elif isinstance(error, BaseException):
-        await ctx.send('Unexpected error adding win, try again. If the error persists,'
-                       ' get help here https://discord.gg/9pS2JdC')
-        logger.error(error, exc_info=True)
+# @get_all_wins.error
+# async def get_all_wins_error(ctx, error):
+#     if isinstance(error, BaseException):
+#         await ctx.send('Unexpected error retrieving wins, try again. If the error persists,'
+#                        ' get help here https://discord.gg/9pS2JdC')
+#         logger.error(error, exc_info=True)
 
 
-@client.command(name='Add_Game',
-                description="Adds a game to the database so wins can be recorded for it",
-                brief="Adds a game to the database",
-                aliases=['addgame', 'add_game', 'ag'],
-                )
-async def add_game(ctx, *, name):
-    response = Python.data_storage.add_game_db(ctx, name)
-    await ctx.send(response)
-
-@add_game.error
-async def add_game_error(ctx, error):
-    if isinstance(error, BaseException):
-        await ctx.send('Unexpected error adding game, try again. If the error persists,'
-                       ' get help here https://discord.gg/9pS2JdC')
+# @client.command(name='Add_Win',
+#                 description="Adds a win to the user's total for a game",
+#                 brief="Adds a win to ther user for a game",
+#                 aliases=['addwin', 'add_win', 'aw', 'win'],
+#                 )
+# async def add_win(ctx, member: discord.Member, *, game_name):
+#     response = Python.data_storage.add_win_db(ctx, member, game_name)
+#     await ctx.send(response)
 
 
-@client.command(name='Add_Play',
-                description="Logs a play to a game in the database",
-                brief="Logs a play",
-                aliases=['addplay', 'add_play', 'ap'],
-                )
-async def add_play(ctx, *, name):
-    response = Python.data_storage.add_play_db(ctx, name)
-    await ctx.send(response)
+# @add_win.error
+# async def add_win_error(ctx, error):
+#     if isinstance(error, discord.ext.commands.BadArgument):
+#         await ctx.send('Failed to add win. Make sure you are using the format `!aw [user] [boardgame name]`'
+#                        ' and that you are mentioning the user directly with @.')
+#         logger.error(error, exc_info=True)
+#     elif isinstance(error, discord.ext.commands.MissingRequiredArgument):
+#         await ctx.send('Failed to add win as you did not provide either the user who won or the name of the game. '
+#                        'Make sure you are using the format `!aw [user] [boardgame name]`'
+#                        ' and that you are mentioning the user directly with @.')
+#         logger.error(error, exc_info=True)
+#     elif isinstance(error, BaseException):
+#         await ctx.send('Unexpected error adding win, try again. If the error persists,'
+#                        ' get help here https://discord.gg/9pS2JdC')
+#         logger.error(error, exc_info=True)
 
 
-@add_play.error
-async def add_play_error(ctx, error):
-    if isinstance(error, BaseException):
-        await ctx.send('Unexpected adding play, try again. If the error persists,'
-                       ' get help here https://discord.gg/9pS2JdC')
+# @client.command(name='Add_Game',
+#                 description="Adds a game to the database so wins can be recorded for it",
+#                 brief="Adds a game to the database",
+#                 aliases=['addgame', 'add_game', 'ag'],
+#                 )
+# async def add_game(ctx, *, name):
+#     response = Python.data_storage.add_game_db(ctx, name)
+#     await ctx.send(response)
 
-@client.command(name='Get_Plays',
-                description="Gets all plays either for one game or for all games",
-                brief="Gets all plays",
-                aliases=['getplays', 'get_plays', 'gp'],
-                )
-async def get_plays_db(ctx, *, name=None):
-    response = Python.data_storage.get_plays_db(ctx, name)
-    await ctx.send(response)
+# @add_game.error
+# async def add_game_error(ctx, error):
+#     if isinstance(error, BaseException):
+#         await ctx.send('Unexpected error adding game, try again. If the error persists,'
+#                        ' get help here https://discord.gg/9pS2JdC')
 
-@get_plays_db.error
-async def get_plays_db_error(ctx, error):
-    if isinstance(error, BaseException):
-        await ctx.send('Unexpected error retrieving plays, try again. If the error persists,'
-                       ' get help here https://discord.gg/9pS2JdC')
+
+# @client.command(name='Add_Play',
+#                 description="Logs a play to a game in the database",
+#                 brief="Logs a play",
+#                 aliases=['addplay', 'add_play', 'ap'],
+#                 )
+# async def add_play(ctx, *, name):
+#     response = Python.data_storage.add_play_db(ctx, name)
+#     await ctx.send(response)
+
+
+# @add_play.error
+# async def add_play_error(ctx, error):
+#     if isinstance(error, BaseException):
+#         await ctx.send('Unexpected adding play, try again. If the error persists,'
+#                        ' get help here https://discord.gg/9pS2JdC')
+
+# @client.command(name='Get_Plays',
+#                 description="Gets all plays either for one game or for all games",
+#                 brief="Gets all plays",
+#                 aliases=['getplays', 'get_plays', 'gp'],
+#                 )
+# async def get_plays_db(ctx, *, name=None):
+#     response = Python.data_storage.get_plays_db(ctx, name)
+#     await ctx.send(response)
+
+# @get_plays_db.error
+# async def get_plays_db_error(ctx, error):
+#     if isinstance(error, BaseException):
+#         await ctx.send('Unexpected error retrieving plays, try again. If the error persists,'
+#                        ' get help here https://discord.gg/9pS2JdC')
 
 
 @client.command(name='HowToPlay',
@@ -195,6 +229,14 @@ async def get_plays_db_error(ctx, error):
 async def youtube_how_to(ctx, *, game_name):
     main_response = Python.YouTube.how_to_play(game_name)
     await ctx.send(main_response)
+
+@youtube_how_to.error
+async def youtube_how_to_error(ctx, error):
+    if isinstance(error, BaseException):
+        await ctx.send('Unexpected error, try again. If the error persists,'
+                       ' get help here https://discord.gg/9pS2JdC')
+        logger.error(error, exc_info=True)        
+
 
 
 @client.command()
@@ -212,6 +254,13 @@ async def get_hot_games(ctx):
     response = Python.BGG.hot_games()
     await ctx.send(response)
 
+@get_hot_games.error
+async def get_hot_games_error(ctx, error):
+    if isinstance(error, BaseException):
+        await ctx.send('Unexpected error, try again. If the error persists,'
+                       ' get help here https://discord.gg/9pS2JdC')
+        logger.error(error, exc_info=True)      
+
 
 @client.command(name='GetHotCompanies',
                 description="Returns BoardGameGeeks current hot board game companies",
@@ -221,6 +270,14 @@ async def get_hot_games(ctx):
 async def get_hot_companies(ctx):
     response = Python.BGG.hot_companies()
     await ctx.send(response)
+
+@get_hot_companies.error
+async def get_hot_companies_error(ctx, error):
+    if isinstance(error, BaseException):
+        await ctx.send('Unexpected error, try again. If the error persists,'
+                       ' get help here https://discord.gg/9pS2JdC')
+        logger.error(error, exc_info=True)      
+
 
 
 @client.command(name='AskQuestion',
@@ -235,6 +292,13 @@ async def ask(ctx, *, arg):
     response = Python.BGG.search_stackexchange(game, question)
     await ctx.send(response)
 
+@ask.error
+async def ask_error(ctx, error):
+    if isinstance(error, BaseException):
+        await ctx.send('Unexpected error, try again. If the error persists,'
+                       ' get help here https://discord.gg/9pS2JdC')
+        logger.error(error, exc_info=True)      
+
 
 @client.command(name='Lookup_BGG_User',
                 description='Lookup BGG user',
@@ -243,8 +307,15 @@ async def ask(ctx, *, arg):
                 )
 async def lookup_bgg_user(ctx, name):
     response = Python.BGG.user_lookup(name)
-    await ctx.send("Games that " + name + " owns: \n" + response)
+    await ctx.send("Games that " + name + " owns: \n\n" + response)
+        
 
+@lookup_bgg_user.error
+async def lookup_bgg_user_error(ctx, error):
+    if isinstance(error, BaseException):
+        await ctx.send('Unexpected error, try again. If the error persists,'
+                       ' get help here https://discord.gg/9pS2JdC')
+        logger.error(error, exc_info=True)              
 
 @client.command(name="Dice_Roll",
                 description="Returns the value of a dice roll number is specified by command",
@@ -253,7 +324,14 @@ async def lookup_bgg_user(ctx, name):
                 )
 async def dice_roll(ctx, sides):
     dice_roll = Python.Dice.dice(int(sides))
-    await ctx.send("The " + str(sides) + " sided die resulted in: " + str(dice_roll))               
+    await ctx.send("The " + str(sides) + " sided die resulted in: " + str(dice_roll))  
+
+@dice_roll.error
+async def dice_roll_error(ctx, error):
+    if isinstance(error, BaseException):
+        await ctx.send('Unexpected error, try again. If the error persists,'
+                       ' get help here https://discord.gg/9pS2JdC')
+        logger.error(error, exc_info=True)                
 
 
 @client.command(name='Game_Ambiance',
@@ -266,6 +344,13 @@ async def game_ambiance_playlist(ctx, *, topic):
     await ctx.send("Here's the result for " + topic +
                    " ambiance \n" + main_response)
 
+@game_ambiance_playlist.error
+async def game_ambiance_playlist_error(ctx, error):
+    if isinstance(error, BaseException):
+        await ctx.send('Unexpected error, try again. If the error persists,'
+                       ' get help here https://discord.gg/9pS2JdC')
+        logger.error(error, exc_info=True)                            
+
 
 @client.command(name='Next_Video',
                 description="Returns the next video in the last youtube search",
@@ -275,6 +360,13 @@ async def game_ambiance_playlist(ctx, *, topic):
 async def next_video(ctx):
     response = Python.YouTube.search_next_video()
     await ctx.send("Next video: \n" + response)
+
+@next_video.error
+async def next_video_error(ctx, error):
+    if isinstance(error, BaseException):
+        await ctx.send('Unexpected error, try again. If the error persists,'
+                       ' get help here https://discord.gg/9pS2JdC')
+        logger.error(error, exc_info=True)      
 
 
 @client.event
